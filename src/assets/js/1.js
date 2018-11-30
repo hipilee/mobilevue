4c4824b921066c1b25638423d0a738967b40dfc1
git <l>
2018-11-30 21:38:22 +0800

    修改 修改了ajax请求，解决了跨域访问时ajax未携带sessionid的问题
    近期待完成
    注册 登陆 绑定MT4账号页面交互和后台系统的整合
    
    远期待完成

--- src/assets/js/1.js
+++ src/assets/js/1.js
@@ -1,16 +1,20 @@
 indexChange = function () {
 
   alert("测试成功131");
+
+
+
+
   $("ul.tradenav").on("click", "li", function () {
 
     alert($(this).text());
     var current_class_name = $(this).attr("class");
-    if ("tactive" != current_class_name){
+    if ("tactive" != current_class_name) {
       $('ul.tradenav>li').removeClass('tactive');
-      $(this).attr("class","tactive");
+      $(this).attr("class", "tactive");
 
     }
-      });
+  });
   //
 }
 
